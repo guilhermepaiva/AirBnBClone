c8902dd2583f3f84275ae7e441709adb29028620
Guilherme Paiva <guilhermepaiva9@gmail.com>
2023-10-22 12:38:15 -0300

    Initial Commit

---
+++ AirBnBClone/AirBnBCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBCloneApp.swift
+//  AirBnBClone
+//
+//  Created by Guilherme Paiva on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
