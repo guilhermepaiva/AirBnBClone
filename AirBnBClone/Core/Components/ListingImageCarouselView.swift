744c23091c8327d2ed45868bf45597bd64caf71a
Guilherme Paiva <guilhermepaiva9@gmail.com>
2023-11-02 18:54:28 -0300

    Listing Detail View -  part 1

---
+++ AirBnBClone/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  AirBnBClone
+//
+//  Created by Guilherme Paiva on 31/10/2023.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
